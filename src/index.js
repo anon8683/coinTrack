1c78921df5eaa28d5f99020abb726cd915e29bec
unknown <flightbuzz1234@gmail.com>
2023-04-12 16:41:47 +0100

    added top line of main content, portfolio value, 24 hour trend and button for adding symbol

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import header from "./header";
+import main from "./main";
 import createLayout from "./layout";
 import "./style.css";
 
-createLayout();
-header();
+createLayout(); //creates grid that containers header,main,footer
+header(); // adds header
+main();
 
-const button = document.getElementById("portfolioButton");
-button.addEventListener("click", function () {
-	console.log("clicked");
-});
+// when user clicks + button,
+// get the amount of holdings and SYMOBL
+// fetch data from api of that SYMBOL
+// calculte holding value, calc total portfolio value
---
+++ src/main.js
@@ -0,0 +1,16 @@
+function main() {
+	const main = document.getElementsByTagName("main")[0];
+	main.innerHTML = `<div>left hand stuff</div>
+    <div class="portfolioSide">
+        <div class="mainTop">
+            <div class="topLeftSide">
+                <h1 id="totalValue">$5,234.23</h1>
+                <div id="trendingTriangle">▲</div>
+                <h4 id="trendAmount">$252.20 (0.25%)</h4>
+            </div>
+            <button id="addHoldingButton">+</button>
+        </div>
+    </div>`;
+}
+
+export default main;
