ad6cf7b437b00e93b3594f92fbb5286752aab378
unknown <flightbuzz1234@gmail.com>
2023-06-05 01:55:31 +0100

    added working fetches for 24 hour ago prices

--- src/addCard.js
+++ src/addCard.js
@@ -89,6 +89,7 @@ function card() {
 		)
 			.then((response) => response.json())
 			.then((data) => {
+				console.log(data);
 				let price = +data.price;
 				console.log(price);
 				if (price > 5) {
@@ -104,10 +105,10 @@ function card() {
 				const pnl = value - holdings[x].price * quantity; // your pnl
 				total += price * quantity; //adds value to portfolio value
 
-				valueBox.textContent = `$${numberWithCommas(value.toFixed(2))}`;
+				valueBox.textContent = `$${numberWithCommas(value.toFixed(1))}`;
 				pnlBox.textContent = `$${numberWithCommas(pnl.toFixed(2))}`;
 				priceBox.textContent = `$${numberWithCommas(price)}`;
-				totalValue.textContent = `$${numberWithCommas(total.toFixed(1))}`;
+				totalValue.textContent = `$${numberWithCommas(total.toFixed(0))}`;
 
 				if (pnl < 0) {
 					//you are negative, add loss class
@@ -120,8 +121,49 @@ function card() {
 	}
 
 	fetchData();
+	yesterdayPrices();
+	async function yesterdayPrices() {
+		const coinsUrl = "https://api.coingecko.com/api/v3/coins/list";
+
+		const coinsResponse = await fetch(coinsUrl);
+
+		const coinsData = await coinsResponse.json();
+		const coin = coinsData.find(
+			(coin) => coin.symbol.toLowerCase() === ticker.toLowerCase()
+		);
+
+		fetch(
+			`https://api.coingecko.com/api/v3/coins/${
+				coin.id
+			}/history?date=${getYesterdayDate()}&localization=false`
+		)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data.market_data.current_price.usd);
+				return data.market_data.current_price.usd;
+			});
+	}
+	function getYesterdayDate() {
+		// work out price 24 hours ago
+
+		let yesterday24 = Math.floor(new Date().getTime() / 1000.0) - 86400; //current time - 24 hours
+		const date = new Date(yesterday24 * 1000);
+
+		let day = date.getDate();
+		let month = date.getMonth();
+		let year = date.getFullYear();
+
+		//our formatted date for api
+		const yesterdayDate = `${day}-${month + 1}-${year}`;
+
+		return yesterdayDate;
+	}
 
 	function numberWithCommas(x) {
+		if (x < 1 && x > 0) {
+			return x;
+		}
+
 		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 	}
 }
